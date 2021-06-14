9aa8a5db40e1fa8d8d8be0dad176dda20f270d69
DefenderGB <defender.gb@gmail.com>
2021-06-14 09:54:44 -0400

    Added suggestions. Cleaned up print. Moved output to a directory.

--- nmapscan.sh
+++ nmapscan.sh
@@ -15,17 +15,17 @@ if [[ $# -eq 0 ]] ; then
 	echo "Usage example: sudo ./nmapscan.sh 10.10.10.1"
 	exit 1
 else
-	echo "[+] Target: $1"
+	echo "Target: $1"
 fi
 
-#Get todays date, will use in filename
-DATE=`date +%m-%d`
-filename=$1_$DATE
+#Create nmap directory
+mkdir -p nmap
 
 #Port scan all ports, exports a nmap and port.txt file
 echo ""
-echo "[+] Running 0-65535 port scan..."
-nmap -p 0-65535 --min-rate=1000 -T4 $1 -o portscan-$filename| grep ^[0-9] | cut -d "/" -f 1 | tr "\n" "," | sed s/,$// > ports.txt
+echo "Running 0-65535 port scan..."
+nmap -p 0-65535 --min-rate=1000 -T4 $1 -o nmap/portscan-$1| grep ^[0-9] | cut -d "/" -f 1 | tr "\n" "," | sed s/,$// > ports.txt
+echo "[+] Output in nmap/portscan-$1"
 
 #Presenting open ports
 echo ""
@@ -33,22 +33,35 @@ echo "[+] Following ports are open:"
 ports="$(cat ports.txt)"
 
 if [[ "$ports" -eq 0 ]] ; then
-	echo "[-] No ports found. Try to run script using sudo OR add -Pn to nmap scans on this script."
+	echo "[-] No ports found open. Try to run script using sudo OR add -Pn to nmap scans on this script."
 	echo "Usage example: sudo ./nmapscan.sh $1"
 	exit 1
 else
 	echo "$(cat ports.txt)"
 fi
 
-#Recommendation
+#Default Script and Version Scan against open ports
 echo ""
-echo "[+] Suggestions"
+echo "Running nmap default scripts (sC) and version scan (sV) on open ports:"
+echo "nmap -T4 -sV -sC -p $ports $1"
+nmap -T4 -sV -sC -p $ports $1 -o nmap/scriptscan-$1 > full.txt
+full="$(cat full.txt)"
+echo "[+] Output in nmap/scriptscan-$1"
+
+#Suggestions
+echo ""
+echo "[+] Enumeration suggestions"
 
 if [[ "$ports" == *"80"* || "$ports" = *"8080"* ]] ; then
+	mkdir -p gobuster
 	echo "===Port 80/8080 (HTTP)==="
-	echo "[Suggest] Force Browse using gobuster:"
-    echo "sudo gobuster dir -u http://$1:80/ -w /opt/SecLists/Discovery/Web-Content/common.txt"
-    echo "sudo gobuster dir -u http://$1:80/ -w /opt/SecLists/Discovery/Web-Content/raft-small-words.txt -x html,php,txt"
+	echo "[Suggest] Run gobuster scans against web servers:"
+	if [[ "$ports" = *"8080"* ]] ; then
+		echo "sudo gobuster dir -u http://$1:8080/ -w /opt/SecLists/Discovery/Web-Content/common.txt -o gobuster-common.txt"
+		echo "sudo gobuster dir -u http://$1:8080/ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x html,php,txt,tar,bak,gz,zip,jpg,png,pdf -o gobuster-full.txt"
+	if [[ "$ports" = *"80"* ]] ; then
+		echo "sudo gobuster dir -u http://$1/ -w /opt/SecLists/Discovery/Web-Content/common.txt -o gobuster-common.txt"
+    	echo "sudo gobuster dir -u http://$1/ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x html,php,txt,tar,bak,gz,zip,jpg,png,pdf -o gobuster-full.txt"
     echo "[Suggest] SQLi: (Capture request (burp) that may be vulnerable to SQLi and use)" 
     echo "sqlmap -r login.req --batch"
     echo "[Suggest] Nikto VulnScan:"
@@ -56,14 +69,23 @@ if [[ "$ports" == *"80"* || "$ports" = *"8080"* ]] ; then
 fi
 
 if [[ "$ports" == *"443"* ]] ; then
+	mkdir -p gobuster
 	echo "===Port 443 (HTTPS)==="
 	echo "[Suggest] Force Browse using gobuster (use -k to skip TLS verification check):"
-    echo "sudo gobuster dir -u https://$1/ -w /opt/SecLists/Discovery/Web-Content/common.txt"
-	echo "sudo gobuster dir -u https://$1/ -w /opt/SecLists/Discovery/Web-Content/raft-small-words.txt -x html,php,txt"
+    echo "sudo gobuster dir -u https://$1/ -w /opt/SecLists/Discovery/Web-Content/common.txt -o gobuster-https-common.txt"
+	echo "sudo gobuster dir -u https://$1/ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x html,php,txt,tar,bak,gz,zip,jpg,png,pdf -o gobuster-https-full.txt"
 	echo "[Suggest] Look over SSL certificate:"
 	echo "openssl s_client -connect $1:443 -showcerts"
 	echo "[Suggest] Look over SAN certificate:"
 	echo "openssl s_client -connect $1:443 | openssl x509 -noout -text | grep DNS:"
+fi
+
+if [[ "$full" == *"wordpress"* ]] ; then
+	echo "===Wordpress Detected==="
+	echo "[Suggest] Run wpscan:"
+	echo "wpscan --url http://$1 --enumerate ap,at,cb,dbe -o output -f cli-no-color"
+	echo "[Suggest] Possible bruteforce:"
+	echo "wpscan --url http://$1  --passwords /usr/share/wordlists/rockyou.txt --usernames admin --max-threads 50"
 fi
 
 if [[ "$ports" == *"119"* ]] ; then
@@ -79,7 +101,7 @@ if [[ "$ports" == *"139"* || "$ports" = *"445"* ]] ; then
 	echo "[Suggest] Use smbclient to list shares:"
 	echo "smbclient -N -L //$1//"
 	echo "[Suggest] Use smbmap to list shares:"
-	echo "smbmap -H $1"
+	echo "smbmap -H $1 -u anonymous -r --depth 5"
 	echo "[Suggest] NULL session:"
 	echo "rpcclient -U \" \" -N $1"
     echo "[Suggest] Show open NFS shares:"
@@ -95,6 +117,8 @@ if [[ "$ports" == *"8009"* ]] ; then
 	echo "[Suggest] Check for GhostCat exploit:"
 	echo "git clone https://github.com/00theway/Ghostcat-CNVD-2020-10487"
 	echo "python3 ajpShooter.py http://$1:8080/ 8009 /blog/home.jsp read"
+	echo "[Suggest] Exploit James Apache 2.3.2:"
+	echo "python /usr/share/exploitdb/exploits/linux/remote/35513.py $1"
 fi
 
 if [[ "$ports" == *"1521"* ]] ; then
@@ -104,16 +128,15 @@ if [[ "$ports" == *"1521"* ]] ; then
 	echo "Find default credentials here: https://book.hacktricks.xyz/pentesting/1521-1522-1529-pentesting-oracle-listener#default-passwords"
 fi
 
-#Default Script and Version Scan against open ports
 echo ""
-echo "[+] Running Default Scripts and Version scan on open ports:"
-echo ""
-nmap -T4 -sV -sC -p $ports $1 -o scriptscan-$filename
-
-echo ""
-echo "[Suggest] Running Vuln Script Nmap Scan:"
-echo "nmap -T4 -sV --script vuln -p $ports $1 -o vulnscan-$filename"
+echo "Running nmap vulnerable script scan on open ports:"
+echo "nmap -T4 -sV --script vuln -p $ports $1"
+nmap -T4 -sV --script vuln -p $ports $1 -o nmap/vulnscan-$1 > vuln.txt
+vuln="$(cat vuln.txt)"
+echo "[+] Output in nmap/vulnscan-$1"
 
 #Clean up
 rm ports.txt
+rm full.txt
+rm vuln.txt
 exit 0
