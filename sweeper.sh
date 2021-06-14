8b68ed9049171735fef4545a8352262609020c9a
DefenderGB <defender.gb@gmail.com>
2021-06-14 09:53:45 -0400

    Added IPv6 multicast target finder and cleaned up output of overall script.

--- sweeper.sh
+++ sweeper.sh
@@ -21,19 +21,28 @@ else
 	echo "[+] Target Network: $1"
 fi
 
-#Nmap Scan with output
-nmap -PEPM -sP -n $1 -oG targets 1>/dev/null
+#Create directory to store files
+mkdir -p nmap
+
+#Nmap Scan without output
+nmap -PEPM -sP -n $1 -oG nmap/targets 1>/dev/null
 
 #Set variable
-ips="$(cat targets)"
+ips="$(cat nmap/targets)"
 
 #Print Results
-echo "[+] Found Targets: (Comma seperated)"
+echo "Found Targets: (Comma seperated)"
 echo $ips | tr "()" "\n" | grep 'Up Host' | cut -d ":" -f 3 | tr -d ' ' | tr "\\n" "," | sed 's/.$//' | sed -e '$a\'
-echo "[+] Found Targets: (Columns)"
+echo "Found Targets: (Columns)"
 echo $ips | tr "()" "\n" | grep 'Up Host' | cut -d ":" -f 3 | tr -d ' ' | column
-echo "[+] Found Targets: (List)"
+echo "Found Targets: (List)"
 echo $ips | tr "()" "\n" | grep 'Up Host' | cut -d ":" -f 3 | tr -d ' '
 targetstring="$(echo '[+] Total Found Targets: ')"
 totalstring="$(echo [+] Total Found Targets:  $ips | tr '()' '\n' | tail -2 | head -1 | cut -d ' ' -f 1)"
 echo $targetstring $totalstring
+
+echo ""
+echo "[+] Found IPv6 Targets on Tun0 interface:"
+nmap -6 --script=targets-ipv6-multicast-echo.nse --script-args 'newtargets,interface=tun0' -sL -o nmap/ip6-tun0-targets.txt
+ip6s="$(cat ip6-tun0-targets)"
+echo $ip6s | grep MAC | cut -f 3- -d ' ' | sed 's/^ //' | cut -d ' ' -f -6
