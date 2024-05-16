84b1914081da4b878b648a0b0267e05f2bd38c53
Gustavo Bobbio-Hertog <61886533+DefenderGB@users.noreply.github.com>
2024-05-15 23:19:19 -0400

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -2,20 +2,20 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 apt update
 apt upgrade
-apt install -y nmap proxychains sshuttle pipx snapd python3-pip python3-venv python3-poetry xfce4 xfce4-goodies xfce4-settings tightvncserver apt-transport-https sublime-text golang hydra dnsrecon dnsenum smbmap droopescan ruby-dev ruby-rubygem
+apt install -y nmap proxychains sshuttle pipx snapd python3-pip python3-venv python3-poetry xfce4 xfce4-goodies xfce4-settings tightvncserver apt-transport-https sublime-text golang hydra dnsrecon dnsenum smbmap ruby-dev terminator
 rm /usr/lib/python3.11/EXTERNALLY-MANAGED
-snap install metasploit-framework
-snap install evil-winrm
-pip3 install pyftpdlib setuptools colorama termcolor service_identity ldap3 kerbrute hydra-core
-pipx install impacket
+#If on Ubuntu uncomment 3 below
+#snap install metasploit-framework # otherwise follow https://docs.rapid7.com/metasploit/installing-the-metasploit-framework/#installing-the-metasploit-framework-on-linux
+#snap install evil-winrm # otherwise gem install evil-winrm
+pip3 install pyftpdlib setuptools colorama termcolor service_identity ldap3 hydra-core droopescan
+pipx install impacket crackmapexec
 go install github.com/OJ/gobuster/v3@latest
 go install github.com/ffuf/ffuf/v2@latest
 export PATH=$PATH:$(go env GOPATH)/bin
-snap install crackmapexec
 mkdir /opt/seclist
 mkdir /opt/scripts
-current_build=$(curl -s https://github.com/carlospolop/PEASS-ng/releases | grep -i "refs/heads/master" -m 1 | awk '{ print $5 }' | cut -d "<" -f1)
-releases_url="https://github.com/carlospolop/PEASS-ng/releases/download/$current_build"
+current_build=$(curl -s https://github.com/peass-ng/PEASS-ng/releases | grep -i "refs/heads/master" -m 1 | awk '{ print $5 }' | cut -d "<" -f1)
+releases_url="https://github.com/peass-ng/PEASS-ng/releases/download/$current_build"
 wget -q $releases_url/linpeas.sh -O /opt/scripts/linpeas.sh
 wget -q $releases_url/winPEASany.exe  -O /opt/scripts/winPEASany.exe
 wget https://github.com/danielmiessler/SecLists/archive/master.zip -O /tmp/SecList.zip
